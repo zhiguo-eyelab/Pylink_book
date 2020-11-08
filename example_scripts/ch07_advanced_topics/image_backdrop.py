3495caec0ff270bfb86dbdbe85e79e25af7600de
Zhiguo Wang <mianwangmign@gmail.com>
2020-11-08 06:26:35 -0500

    update

--- example_scripts/ch07_advanced_topics/image_backdrop.py
+++ example_scripts/ch07_advanced_topics/image_backdrop.py
@@ -1,44 +1,74 @@
 # Filename: image_backdrop.py
+# Author: Zhiguo Wang
+# Date: 11/7/2020
+#
+# Description:
+# Transfer an image to the Host to use as the backdrop
 
 import pylink
 from PIL import Image
 
-# open a connection to the tracker
+# Connect to the tracker
 tk = pylink.EyeLink('100.1.1.1')
-# set the tracker to offline mode
+
+# Put the tracker in offline mode before we transfer the image
 tk.setOfflineMode()
 
-# convert an image to the <pixel> format supported by the bitmapBackdrop command
-im = Image.open('sacrmeto.bmp') # open an image with PIL
-w,h = im.size # get the width and height of an image
-pixels = im.load() # load to access pixel data
-pixels_2transfer = [[pixels[i,j] for i in range(w)] for j in range(h)]
+# convert an image to the <pixel> format supported by
+# the bitmapBackdrop command
+im = Image.open('sacrmeto.bmp')  # open an image with PIL
+w, h = im.size  # get the width and height of an image
+pixels = im.load()  # access the pixel data
+pixels_img = [[pixels[i, j] for i in range(w)] for j in range(h)]
 
-# construct an image in <pixel> format with RGB tuples
-pixels_RGB = [[(0,0,0), (0,0,0),(0,0,0),(255,255,255),(255,255,255),(255,255,255)],
-                [(0,0,0), (0,0,0),(0,0,0),(255,255,255),(255,255,255),(255,255,255)],
-                [(0,0,0), (0,0,0),(0,0,0),(255,255,255),(255,255,255),(255,255,255)],
-                [(255,255,255), (255,255,255),(255,255,255),(0,0,0),(0,0,0),(0,0,0)],
-                [(255,255,255), (255,255,255),(255,255,255),(0,0,0),(0,0,0),(0,0,0)],
-                [(255,255,255), (255,255,255),(255,255,255),(0,0,0),(0,0,0),(0,0,0)]]
+# Construct an image in <pixel> format with RGB tuples
+white_rgb = (255, 255, 255)
+black_rgb = (0, 0, 0)
+pixels_rgb = [
+    [black_rgb, black_rgb, black_rgb, white_rgb, white_rgb, white_rgb],
+    [black_rgb, black_rgb, black_rgb, white_rgb, white_rgb, white_rgb],
+    [black_rgb, black_rgb, black_rgb, white_rgb, white_rgb, white_rgb],
+    [white_rgb, white_rgb, white_rgb, black_rgb, black_rgb, black_rgb],
+    [white_rgb, white_rgb, white_rgb, black_rgb, black_rgb, black_rgb],
+    [white_rgb, white_rgb, white_rgb, black_rgb, black_rgb, black_rgb],
+    ]
 
 # construct an image in <pixel> format with hexadecimal values alpha-R-G-B
-pixels_HEX = [[0x0,0x0,0x0,0x00FFFFFF,0x00FFFFFF,0x00FFFFFF],
-                [0x0,0x0,0x0,0x00FFFFFF,0x00FFFFFF,0x00FFFFFF],
-                [0x0,0x0,0x0,0x00FFFFFF,0x00FFFFFF,0x00FFFFFF],
-                [0x00FFFFFF,0x00FFFFFF,0x00FFFFFF,0x0,0x0,0x0],
-                [0x00FFFFFF,0x00FFFFFF,0x00FFFFFF,0x0,0x0,0x0],
-                [0x00FFFFFF,0x00FFFFFF,0x00FFFFFF,0x0,0x0,0x0]]
+white_hex = 0x00FFFFFF
+black_hex = 0x0
+pixels_hex = [
+    [black_hex, black_hex, black_hex, white_hex, white_hex, white_hex],
+    [black_hex, black_hex, black_hex, white_hex, white_hex, white_hex],
+    [black_hex, black_hex, black_hex, white_hex, white_hex, white_hex],
+    [white_hex, white_hex, white_hex, black_hex, black_hex, black_hex],
+    [white_hex, white_hex, white_hex, black_hex, black_hex, black_hex],
+    [white_hex, white_hex, white_hex, black_hex, black_hex, black_hex],
+    ]
 
-# transfer one of the above images to the Host PC screen
+# Transfer the images to the Host PC screen
 tk.sendCommand('clear_screen 0')
-#tk.bitmapBackdrop(w, h, pixels_2transfer, 0, 0, w, h, 0, 0, pylink.BX_MAXCONTRAST)
-tk.bitmapSaveAndBackdrop(w, h, pixels_2transfer, 0, 0, w, h, 'trial_image', 'img',
+tk.bitmapBackdrop(w, h, pixels_img, 0, 0, w, h, 0, 0, pylink.BX_MAXCONTRAST)
+tk.bitmapSaveAndBackdrop(w, h, pixels_img, 0, 0, w, h,
+                         'trial_image', 'img',
                          pylink.SV_MAKEPATH, 0, 0, pylink.BX_MAXCONTRAST)
-                         
-#tk.bitmapBackdrop(6, 6, pixels_HEX, 0, 0, 6, 6, 0, 0, pylink.BX_MAXCONTRAST)
-#tk.bitmapSaveAndBackdrop(6, 6, pixels_HEX, 0, 0, 6, 6, 'trial_image', 'img',
-#                         pylink.SV_MAKEPATH, 300, 300, pylink.BX_MAXCONTRAST)
 
-# close the connection
+# Show the image for 1-sec on the Host PC
+pylink.msecDelay(1000)
+
+# Transfer the checkboard constructed with Hex values to the Host
+tk.sendCommand('clear_screen 0')
+tk.bitmapBackdrop(6, 6, pixels_hex, 0, 0, 6, 6, 0, 0, pylink.BX_MAXCONTRAST)
+tk.bitmapSaveAndBackdrop(6, 6, pixels_hex, 0, 0, 6, 6, 'trial_image', 'img',
+                         pylink.SV_MAKEPATH, 300, 300, pylink.BX_MAXCONTRAST)
+
+# Show the checkboard for 1-sec on the Host PC
+pylink.msecDelay(1000)
+
+# Transfer the checkboard constructed with RGB tuples to the Host
+tk.sendCommand('clear_screen 0')
+tk.bitmapBackdrop(6, 6, pixels_rgb, 0, 0, 6, 6, 0, 0, pylink.BX_MAXCONTRAST)
+tk.bitmapSaveAndBackdrop(6, 6, pixels_rgb, 0, 0, 6, 6, 'trial_image', 'img',
+                         pylink.SV_MAKEPATH, 300, 300, pylink.BX_MAXCONTRAST)
+
+# Close the connection
 tk.close()
--- example_scripts/ch07_advanced_topics/recording_playback.py
+++ example_scripts/ch07_advanced_topics/recording_playback.py
@@ -1,92 +1,105 @@
 # Filename: recording_playback.py
+# Author: Zhiguo Wang
+# Date: 11/7/2020
+#
+# Description:
+# Playback the recording from the previous trial
 
 import pylink
 import pygame
 from math import hypot
 
-# connect to the tracker
+# Monitor resolution
+SCN_WIDTH, SCN_HEIGHT = (800, 600)
+
+# Connect to the tracker
 tk = pylink.EyeLink('100.1.1.1')
 
-# send screen coordinates to the tracker
-tk.sendCommand('screen_pixel_coords 0 0 800 600')
+# Send screen pixel coordinates to the tracker
+tk.sendCommand('screen_pixel_coords 0 0 %d %d' % (SCN_WIDTH, SCN_HEIGHT))
 
-# open a Pygame window to force openGraphics() to use a
+# Open a Pygame window to force openGraphics() to use a
 # non-fullscreen window
-win = pygame.display.set_mode((800, 600)) 
+win = pygame.display.set_mode(SCN_WIDTH, SCN_HEIGHT)
 pylink.openGraphics()
 
-# open a data file, so we can playback the recorded data
+# Open an EDF data file on the Host
 tk.openDataFile('playback.edf')
 
-# calibrate the tracker, one can run in mouse simulation mode
+# Calibrate the tracker
 tk.doTrackerSetup()
 
-# run 3 trials
+# Run through 3 trials
 for i in range(3):
+    # Start recording
+    tk.startRecording(1, 1, 1, 1)
 
-    # start recording
-    tk.startRecording(1,1,1,1)
-    
-    # a grey fixation, a green target, and a bright distractor
+    # Show a grey fixation, a green target, and a bright distractor
     # on a black background
-    win.fill((0,0,0))
-    pygame.draw.circle(win, (128,128,128), (400,300), 10)
-    pygame.draw.circle(win, (0,255,0), (400,100), 20)
-    pygame.draw.polygon(win, (255,255,255), \
-                        [(521,159),(541,139),(561,159),(541,179)])
-    pygame.display.flip()    
+    win.fill((0, 0, 0))
+    pygame.draw.circle(win, (128, 128, 128), (400, 300), 10)
+    pygame.draw.circle(win, (0, 255, 0), (400, 100), 20)
+    pygame.draw.polygon(win, (255, 255, 255),
+                        [(521, 159), (541, 139), (561, 159), (541, 179)])
+    pygame.display.flip()
 
-    # wait for a saccadic response
-    got_SAC = False
-    while not got_SAC:
+    # Wait for a saccade towards the green target
+    got_sac = False
+    while not got_sac:
         dt = tk.getNextData()
         if dt == pylink.ENDSACC:
             ev_data = tk.getFloatData()
-            amp_x, amp_y =  ev_data.getAmplitude()
-            # jump out of the loop if a saccade >2 deg is detected
+            amp_x, amp_y = ev_data.getAmplitude()
+            # Break if a saccade > 2 deg is detected
             if hypot(amp_x, amp_y) > 2.0:
-                got_SAC = True
+                got_sac = True
 
-    tk.stopRecording() # stop recording
+    tk.stopRecording()  # stop recording
 
-    # start playback
+    # Start playback
     tk.startPlayBack()
-    # give the Host some time to switch to the playback mode
-    pylink.pumpDelay(50)
-    
+    # Give the Host some time to switch to the playback mode
+    pylink.msecDelay(50)
+
     smp_pos = []
     smp_timestamp = -32768
     while True:
+        # Get the newest sample
         smp = tk.getNewestSample()
         if smp is not None:
-            if smp.getEye() == 0:
+            if smp.getEye() == 0:  # left eye sample
                 gaze_pos = smp.getLeftEye().getGaze()
-            else:
+            else:  # right eye sample
                 gaze_pos = smp.getRightEye().getGaze()
+
+            # Check if the sample is new
             if smp.getTime() > smp_timestamp:
-                smp_pos.append((int(gaze_pos[0]), int(gaze_pos[1])))
+                smp_pos.append((int(gaze_pos[0]),
+                                int(gaze_pos[1])))
                 smp_timestamp = smp.getTime()
 
-                # plot the tracjectory
+                # Plot the tracjectory
                 if len(smp_pos) > 1:
-                    pygame.draw.lines(win, (255,255,255),\
+                    pygame.draw.lines(win, (255, 255, 255),
                                       False, smp_pos, 3)
                     pygame.display.update()
-        
-        # exit when the playback mode ends                    
+
+        # Exit when the playback mode ends
         if tk.getCurrentMode() == pylink.IN_IDLE_MODE:
             break
 
-    # keep the trajectory on screen for a while
+    # Keep the trajectory on the screen for a while
     pylink.pumpDelay(1500)
-    # clear up the screen
-    win.fill((0,0,0)) 
+
+    # Clear up the screen
+    win.fill((0, 0, 0))
     pygame.display.flip()
     pylink.pumpDelay(1000)
 
-    # stop playback            
+    # Stop playback
     tk.stopPlayBack()
-    
+
+# Close the EDF data file, close the link, then quit Pygame
 tk.closeDataFile()
 tk.close()
 pygame.quit()
