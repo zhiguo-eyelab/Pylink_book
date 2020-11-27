5e3bf3111e7076f06e17036ced178e3ba93b0d2c
Zhiguo Wang <mianwangmign@gmail.com>
2020-11-26 19:46:46 -0500

    updated

--- example_scripts/ch02_psychopy/simon_effect.py
+++ example_scripts/ch02_psychopy/simon_effect.py
@@ -1,79 +1,95 @@
 # Filename: simon_effect.py
+# Author: Zhiguo Wang
+# Date: 11/26/2020
+#
+# Description:
+# Measuring the Simon effect in PsychoPy
 
 import random
 from psychopy import visual, core, event, gui
 
-# open a window and create stimuli
+# Open a window and prepare the stimuli
 win = visual.Window((1024, 768), units='pix', fullscr=False, color='black')
 text_msg = visual.TextStim(win, text='message')
 tar_stim = visual.GratingStim(win, tex='None', mask='circle', size=60.0)
 
-# possible target position
-tar_pos = {'left': (-200, 0), 'right':(200, 0)}
+# Possible target position
+tar_pos = {'left': (-200, 0), 'right': (200, 0)}
 
-# trial list
-trials = [['left', 'red', 'z', 'congruent'],
-          ['left', 'blue', 'slash', 'incongruent'],
-          ['right', 'red', 'z', 'incongruent'],
-          ['right', 'blue', 'slash', 'congruent']]
+# A list of all possible trial parameter combinations
+trials = [
+    ['left', 'red', 'z', 'congruent'],
+    ['left', 'blue', 'slash', 'incongruent'],
+    ['right', 'red', 'z', 'incongruent'],
+    ['right', 'blue', 'slash', 'congruent']
+    ]
+
 
 def run_trial(trial_pars, data_file, participant):
     """ Run a single trial.
-    
-    trial_pars -- target position, color and correct key, e.g., ['left', 'red', 'z', 'cong']
-    data_file -- an file to save trial data
-    participant -- information about the participant in a dictionary, {'id':1, 'name':zw}"""
-    
-    pos, color, cor_key, congruency = trial_pars # upacking the parameter list
-    tar_stim.pos = tar_pos[pos] # set target postion
-    tar_stim.color = color # set target color
 
-    # present the fixation for 750 ms
+    trial_pars -- target position, color, and correct key, e.g.,
+                  ['left', 'red', 'z', 'cong']
+    data_file -- a file to save trial data
+    participant -- information about the participant in a dictionary,
+                  {'id':1, 'name':zw}"""
+
+    # Unpacking the parameter list
+    pos, color, cor_key, congruency = trial_pars
+
+    # Set target position and color
+    tar_stim.pos = tar_pos[pos]
+    tar_stim.color = color
+
+    # Present a fixation cross for 750 ms
     text_msg.text = '+'
     text_msg.draw()
     win.flip()
     core.wait(0.750)
 
-    # present the target and wait for a key response
+    # Present the target and wait for a key response
     tar_stim.draw()
     win.flip()
     t_tar_onset = core.getTime()
-    tar_resp = event.waitKeys(1500, ['z', 'slash'],timeStamped=True)
+    tar_resp = event.waitKeys(1500, ['z', 'slash'], timeStamped=True)
 
-    # write data to file 
-    trial_data = list(participant.values()) + trial_pars +[t_tar_onset] + list(tar_resp[0])
-    trial_data = map(str, trial_data) # convert to string
+    # write data to file
+    trial_data = list(participant.values()) + \
+        trial_pars + [t_tar_onset] + \
+        list(tar_resp[0])
+    trial_data = map(str, trial_data)  # convert to string
     data_file.write(','.join(trial_data) + '\n')
-    
+
     # clear the screen and set an ITI of 500 ms
     win.color = 'black'
     win.flip()
     core.wait(0.500)
 
-# -- real experiment starts here --
-# get participant info from a dialog
+# ------ Real experiment starts here -------
+# Get participant info from a dialog
 participant = {'Participant ID': 0, 'Participant Initials': 'zw'}
 dlg = gui.DlgFromDict(participant, title='Enter participant info here')
 
-# open a data file with write permission
+# Open a data file with write permission
 d_file = open(participant['Participant Initials']+'.csv', 'w')
 
-# show the task instructions
+# Show task instructions
 text_msg.text = 'Press Z to RED\nPress / to BLUE\n\nPress <SPACE> to start'
 text_msg.draw()
 win.flip()
 event.waitKeys(keyList=['space'])
 
-# randomly shuffle the trial list and iterate over all of them
-random.seed = 1000
-test_trials = trials[:]*2
-random.shuffle(test_trials)
-for pars in test_trials:
+# Randomly shuffle the trial list and iterate over all of them
+random.seed = 1000  # Set a random seed
+trial_list = trials[:]*2
+random.shuffle(trial_list)
+for pars in trial_list:
     run_trial(pars, d_file, participant)
 
-# close data file
+# Close the data file
 d_file.close()
 
-# exit
+# Quit PsychoPy
+win.close()
 core.quit()
 
