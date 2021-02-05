7d789ce54ed10a6528e10561eeaa36865b162761
Zhiguo Wang <37347513+mianwangming@users.noreply.github.com>
2021-02-05 14:53:05 -0500

    Update image_backdrop.py

--- example_scripts/ch07_advanced_topics/image_backdrop.py
+++ example_scripts/ch07_advanced_topics/image_backdrop.py
@@ -16,11 +16,12 @@ tk = pylink.EyeLink('100.1.1.1')
 # Put the tracker in offline mode before we transfer the image
 tk.setOfflineMode()
 
-# convert an image to the <pixel> format supported by
-# the bitmapBackdrop command
+# convert the image to the <pixel> format supported by
+# the bitmapBackdrop() command
 im = Image.open('sacrmeto.bmp')  # open an image with PIL
 w, h = im.size  # get the width and height of an image
 pixels = im.load()  # access the pixel data
+# reformat the pixels 
 pixels_img = [[pixels[i, j] for i in range(w)] for j in range(h)]
 
 # Construct an image in <pixel> format with RGB tuples
@@ -35,7 +36,8 @@ pixels_rgb = [
     [white_rgb, white_rgb, white_rgb, black_rgb, black_rgb, black_rgb],
     ]*100
 
-# construct an image in <pixel> format with hexadecimal values alpha-R-G-B
+# construct an image in <pixel> format with hexadecimal values
+# i.e., alpha-R-G-B
 white_hex = 0x00FFFFFF
 black_hex = 0x0
 pixels_hex = [
@@ -49,28 +51,29 @@ pixels_hex = [
 
 # Transfer the images to the Host PC screen
 tk.sendCommand('clear_screen 0')
+tk.sendCommand('echo PIXELs_FROM_IMAGE')
 tk.bitmapBackdrop(w, h, pixels_img, 0, 0, w, h,
+                  50, 50, pylink.BX_MAXCONTRAST)
 
-                  0, 0, pylink.BX_MAXCONTRAST)
-
-# Show the image for 1-sec on the Host PC
+# Show the image for 3-sec on the Host PC
 pylink.msecDelay(3000)
 
 # Transfer the checkerboard constructed with Hex values to the Host
-# show it at (200,0) for 3-sec
+# show it at (250, 0) for 3-sec
 tk.sendCommand('clear_screen 0')
-tk.sendCommand('echo PIXELs_IN_HEX')
-tk.bitmapBackdrop(6, 6, pixels_hex, 0, 0, 6, 6,
-                  0, 0, pylink.BX_MAXCONTRAST)
+tk.sendCommand('echo PIXELs_FROM_HEX')
+tk.bitmapBackdrop(6, 600, pixels_hex, 0, 0, 6, 600,
+                  250, 0, pylink.BX_MAXCONTRAST)
 
+# Show the image for 3-sec on the Host PC
 pylink.msecDelay(3000)
 
 # Transfer the checkerboard constructed with RGB tuples to the Host
-# show it at (200, 0) for 3 sec
+# show it at (250, 0) for 3 sec
 tk.sendCommand('clear_screen 0')
-tk.sendCommand('echo PIXELs_IN_RGB')
+tk.sendCommand('echo PIXELs_FROM_RGB')
 tk.bitmapBackdrop(6, 600, pixels_rgb, 0, 0, 6, 600,
-                  200, 0, pylink.BX_MAXCONTRAST)
+                  250, 0, pylink.BX_MAXCONTRAST)
 
 pylink.msecDelay(3000)
 
